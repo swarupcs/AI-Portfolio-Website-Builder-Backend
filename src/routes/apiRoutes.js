abe23e49197d6dfa1cd998956fac45238af9141f
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2025-09-21 16:31:38 +0530

    add resume route to API router

--- src/routes/apiRoutes.js
+++ src/routes/apiRoutes.js
@@ -1,10 +1,12 @@
 import express from "express";
 import authRouter from "./auth.routes.js";
 import profileRouter from "./profile.routes.js";
+import resumeRouter from "./resume.routes.js";
 
 const apiRouter = express.Router();
 
 apiRouter.use("/auth", authRouter);
 apiRouter.use("/profile", profileRouter);
+apiRouter.use("/resume", resumeRouter);
 
 export default apiRouter;
