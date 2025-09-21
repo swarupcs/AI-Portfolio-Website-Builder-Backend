9d9b5cef93e0ecf85ea1bac2ff77a0005099d4a0
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2025-09-21 16:31:04 +0530

    refactor database connection to use MONGO_URI from config module

--- src/config/db.js
+++ src/config/db.js
@@ -1,8 +1,10 @@
 import mongoose from 'mongoose';
+import { MONGO_URI } from './config.js';
+
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
+    await mongoose.connect(MONGO_URI, {
       // Recommended options
       useNewUrlParser: true,
       useUnifiedTopology: true,
